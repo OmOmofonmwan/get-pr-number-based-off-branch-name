94750ba4495d92424066f3a2f3631d844d4dfd36
OmOmofonmwan <omomofonmwan@tinkarbuild.com>
2024-06-11 14:05:14 -0500

    Fixed dependencies

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { Octokit } = require('@octokit/core');
-//import { Octokit } from 'octokit'
+//const { Octokit } = require('@octokit/core');
+import { Octokit } from '@octokit/core'
 
 
 
